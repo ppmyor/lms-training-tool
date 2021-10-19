5913e5f31d7bbae98f987f74c73b8f4d80553659
ppmyor <yorocobe@gmail.com>
2021-10-19 23:52:50 +0900

    feat: 오디오 컨트롤러 기능 구현

--- js/select-bar.js
+++ js/select-bar.js
@@ -8,15 +8,24 @@ const fullScreenButton = document.querySelector(".full-screen-button");
 const resizeFont = document.querySelector(".font-resize");
 const resizeFontSelector = document.querySelector(".font-resize-selector");
 
-function displayviewSelector() {
+// about controller volume control bar
+const audioControl = document.querySelector(".audio-control");
+const audiocontrolSelector = document.querySelector(".audio-control-selector");
+
+function displayViewSelector() {
     viewSelectorDropMenu.classList.toggle("hidden");
     navContents.classList.toggle("hidden");
     fullScreenButton.classList.toggle("hidden");
 }
 
-function displayFontResizeSection() {
+function displayFontResizeSelector() {
     resizeFontSelector.classList.toggle("hidden");
 }
 
-viewSelector.addEventListener("click", displayviewSelector);
-resizeFont.addEventListener("click", displayFontResizeSection);
+function displayAudioControlSelector() {
+    audiocontrolSelector.classList.toggle("hidden");
+}
+
+viewSelector.addEventListener("click", displayViewSelector);
+resizeFont.addEventListener("click", displayFontResizeSelector);
+audioControl.addEventListener("click", displayAudioControlSelector);
--- js/tts-autoplay.js
+++ js/tts-autoplay.js
@@ -1,15 +1,50 @@
-const buttonClicked = document.querySelector(".next-page-btn");
+const nextButton = document.querySelector(".next-page-btn");
+const pauseButton = document.querySelector(".pause-button");
+const playButton = document.querySelector(".play-button");
+const volumeUpButton = document.querySelector(".volume-up");
+const volumeDownButton = document.querySelector(".volume-down");
 
-let variable = 0;
+let pageVariable = 0;
+let volumeVariable = 0.5;
 
-buttonClicked.addEventListener("click", function () {
-    if (variable === 0) {
+const audio = new Audio("./assets/ex-audio.wav");
+
+nextButton.addEventListener("click", function () {
+    if (pageVariable === 0) {
         document.getElementById("working-space").src = "./content-1.html";
-        const audio1 = new Audio("./assets/ex-audio.wav");
-        audio1.play();
-        audio1.autoplay;
-        variable++;
+        audio.volume = volumeVariable;
+        audio.play();
+        audio.autoplay;
+        pageVariable++;
     } else {
         console.log("done");
+    }
+});
+
+pauseButton.addEventListener("click", function () {
+    audio.pause();
+});
+
+playButton.addEventListener("click", function () {
+    audio.play();
+});
+
+volumeUpButton.addEventListener("click", function () {
+    volumeVariable = volumeVariable + 0.1;
+    if (volumeVariable < 1.0) {
+        audio.volume = volumeVariable;
+        console.log(volumeVariable);
+    } else {
+        console.log("maxVolume");
+    }
+});
+
+volumeDownButton.addEventListener("click", function () {
+    volumeVariable = volumeVariable - 0.1;
+    if (volumeVariable > 0) {
+        audio.volume = volumeVariable;
+        console.log(volumeVariable);
+    } else {
+        console.log("minVolume");
     }
 });
