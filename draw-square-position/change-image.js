f813db1d47b897cd0cd50c12aa19d988db6ee3f5
ppmyor <yorocobe@gmail.com>
2021-12-01 14:19:58 +0900

    feat: 비플제로페이 계좌연결, 사용 컨텐츠 추가

--- draw-square-position/change-image.js
+++ draw-square-position/change-image.js
@@ -2,8 +2,8 @@ const changeButton = document.querySelector(".change-btn");
 const imageArray = [];
 
 const codeDescription = document.querySelector(".code-area-desc");
-const pageNum = 28;
-const pageName = "SD23";
+const pageNum = 9;
+const pageName = "SD24";
 
 for (i = 1; i <= pageNum; i++) {
     imageArray.push(`../assets/${pageName}/image/${i}.jpg`);
