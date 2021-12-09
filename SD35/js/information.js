62799990e58110a7f353d21ac87c09994eeba6b6
ppmyor <yorocobe@gmail.com>
2021-12-09 10:17:41 +0900

    feat: SD35 인풋에 임의의 값 입력 시 검증 기능 추가

--- SD35/js/information.js
+++ SD35/js/information.js
@@ -6,22 +6,30 @@ for (j = 1; j <= pageNum; j++) {
     audioArray.push(audio);
 }
 
-const page1 = "서울특별시 공공서비스 예약을 위해서는 로그인이 반드시 필요합니다. 비회원 로그인을 실습하겠습니다. 화면을 눌러서 다음으로 이동해 주세요.";
-const page2 = "로그인을 위해 서울특별시 공공서비스 예약 첫 화면에서 좌측 상단 세 줄 모양을 눌러서 메뉴를 펼쳐보겠습니다. 좌측 상단 서울특별시 로고 아래 세 줄 모양 메뉴 버튼을 눌러주세요.";
+const page1 =
+    "서울특별시 공공서비스 예약을 위해서는 로그인이 반드시 필요합니다. 비회원 로그인을 실습하겠습니다. 화면을 눌러서 다음으로 이동해 주세요.";
+const page2 =
+    "로그인을 위해 서울특별시 공공서비스 예약 첫 화면에서 좌측 상단 세 줄 모양을 눌러서 메뉴를 펼쳐보겠습니다. 좌측 상단 서울특별시 로고 아래 세 줄 모양 메뉴 버튼을 눌러주세요.";
 const page3 = "서비스 메뉴가 열렸습니다. 로그인을 위해 상단 공공서비스 예약 로고 바로 아래 로그인 버튼을 눌러주세요.";
-const page4 = "서울시 통합 아이디로 회원가입을 하면 서비스를 편리하게 이용할 수 있습니다. 네이버 또는 카카오 아이디 로그인도 가능합니다. 실습에서는 비회원 로그인을 진행하겠습니다. 로그인 화면 탭에서 비회원 로그인 버튼을 눌러주세요.";
-const page5 = "휴대폰 본인 인증과 아이핀 본인 인증으로 비회원 로그인이 가능합니다. 실습에서는 휴대폰 본인인증으로 진행하겠습니다. 휴대폰 본인인증에서 본인인증 버튼을 눌러주세요.";
+const page4 =
+    "서울시 통합 아이디로 회원가입을 하면 서비스를 편리하게 이용할 수 있습니다. 네이버 또는 카카오 아이디 로그인도 가능합니다. 실습에서는 비회원 로그인을 진행하겠습니다. 로그인 화면 탭에서 비회원 로그인 버튼을 눌러주세요.";
+const page5 =
+    "휴대폰 본인 인증과 아이핀 본인 인증으로 비회원 로그인이 가능합니다. 실습에서는 휴대폰 본인인증으로 진행하겠습니다. 휴대폰 본인인증에서 본인인증 버튼을 눌러주세요.";
 const page6 = "본인인증을 위해서 이용 중이신 통신사를 선택해 주세요.";
-const page7 = "통신사가 선택되었습니다. 인증을 위한 전체 동의를 진행하겠습니다. 전체 동의 왼쪽 동그라미를 눌러서 선택해 주세요.";
+const page7 =
+    "통신사가 선택되었습니다. 인증을 위한 전체 동의를 진행하겠습니다. 전체 동의 왼쪽 동그라미를 눌러서 선택해 주세요.";
 const page8 = "전체 동의가 선택되었습니다. 화면 하단 문자로 인증하기 버튼을 눌러서 인증을 진행해 주세요.";
-const page9 = "실습에서 입력한 정보는 저장 및 전송되지 않습니다. 문자 인증을 위해 이름을 입력하고 엔터 또는 확인을 눌러주세요.";
+const page9 =
+    "실습에서 입력한 정보는 저장 및 전송되지 않습니다. 문자 인증을 위해 이름을 입력하고 엔터 또는 확인을 눌러주세요.";
 const page10 = "주민번호 앞 생년월일 여섯 자리를 입력하고 엔터 또는 확인을 눌러주세요.";
 const page11 = "주민등록번호 뒤 첫 자리를 입력하고 엔터 또는 확인을 눌러주세요.";
 const page12 = "휴대폰 번호를 입력하고 엔터 또는 확인을 눌러주세요.";
 const page13 = "입력이 완료되었습니다. 진한 회색 확인 버튼을 눌러주세요.";
-const page14 = "문자로 인증번호가 발송됩니다. 인증은 3분 내에 진행해 주셔야 합니다. 실습에서 인증번호는 9,3,1,8,3,8을 입력하겠습니다. 인증번호 9,3,1,8,3,8을 입력하고 엔터 또는 확인을 눌러주세요.";
+const page14 =
+    "문자로 인증번호가 발송됩니다. 인증은 3분 내에 진행해 주셔야 합니다. 실습에서 인증번호는 9,3,1,8,3,8을 입력하겠습니다. 인증번호 9,3,1,8,3,8을 입력하고 엔터 또는 확인을 눌러주세요.";
 const page15 = "인증번호 9,3,1,8,3,8 이 입력되었습니다. 인증 완료를 위해 진한 회색 인증 확인 버튼을 눌러주세요.";
-const page16 = "비회원 로그인이 완료되었습니다. 다음 시간에는 빠른 예약 기능을 활용해서 공공서비스 예약하는 법을 실습하겠습니다.";
+const page16 =
+    "비회원 로그인이 완료되었습니다. 다음 시간에는 빠른 예약 기능을 활용해서 공공서비스 예약하는 법을 실습하겠습니다.";
 
 pageDescArray.push(
     page1,
@@ -43,81 +51,167 @@ pageDescArray.push(
 );
 
 const inputPage = [
-    { page: 9, correctAnswer: "한글2자이상" },
-    { page: 10, correctAnswer: "숫자만6자리" },
-    { page: 11, correctAnswer: "숫자만1자리" },
-    { page: 12, correctAnswer: "숫자만10-11자리" }
-    { page: 15, correctAnswer: "931838" }
+    { page: 8, correctAnswer: "이름" },
+    { page: 9, correctAnswer: "숫자 6자리" },
+    { page: 10, correctAnswer: "숫자 1자리" },
+    { page: 11, correctAnswer: "숫자 10~11자리" },
+    { page: 13, correctAnswer: "931838" },
 ];
+
+function handleInput(pageNumber) {
+    for (i = 0; i < inputPage.length; i++) {
+        if (pageNumber === inputPage[i].page) {
+            DrawInputBox(coordinateArray[pageNumber]);
+            let answer = inputPage[i].correctAnswer;
+            document.querySelector(".input-answer").addEventListener("keydown", function (event) {
+                if (event.keyCode === 13) {
+                    if (pageNumber === 8) {
+                        if (2 <= this.value.length && this.value.length <= 5) {
+                            handleClickBox();
+                        } else {
+                            alert(RETRY_MESSAGE);
+                        }
+                    } else if (pageNumber === 9) {
+                        if (this.value.length === 6) {
+                            handleClickBox();
+                        } else {
+                            alert(RETRY_MESSAGE);
+                        }
+                    } else if (pageNumber === 10) {
+                        if (this.value === "1" || this.value === "2" || this.value === "3" || this.value === "4") {
+                            handleClickBox();
+                        } else {
+                            alert(RETRY_MESSAGE);
+                        }
+                    } else if (pageNumber === 11) {
+                        if (10 <= this.value.length && this.value.length <= 11) {
+                            handleClickBox();
+                        } else {
+                            alert(RETRY_MESSAGE);
+                        }
+                    } else if (pageNumber === 13) {
+                        if (this.value === answer) {
+                            handleClickBox();
+                        } else {
+                            alert(RETRY_MESSAGE);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
 
 const coordinate1 = {
     startRateX: 0,
     startRateY: 0,
     endRateX: 1,
-    endRateY: 1
+    endRateY: 1,
 };
 
 const coordinate2 = {
-    startRateX: 0.0169971671388102, startRateY: 0.3648854961832061, endRateX: 0.9801699716713881, endRateY: 0.6030534351145038
+    startRateX: 0.0169971671388102,
+    startRateY: 0.3648854961832061,
+    endRateX: 0.9801699716713881,
+    endRateY: 0.6030534351145038,
 };
 
 const coordinate3 = {
-    startRateX: 0.0056657223796034, startRateY: 0.1450381679389313, endRateX: 0.2521246458923513, endRateY: 0.2198473282442748
+    startRateX: 0.0056657223796034,
+    startRateY: 0.1450381679389313,
+    endRateX: 0.2521246458923513,
+    endRateY: 0.2198473282442748,
 };
 
 const coordinate4 = {
-    startRateX: 0.4985835694050991, startRateY: 0.33893129770992364, endRateX: 0.9631728045325779, endRateY: 0.39083969465648855
+    startRateX: 0.4985835694050991,
+    startRateY: 0.33893129770992364,
+    endRateX: 0.9631728045325779,
+    endRateY: 0.39083969465648855,
 };
 
 const coordinate5 = {
-    startRateX: 0.0764872521246459, startRateY: 0.5053435114503817, endRateX: 0.9206798866855525, endRateY: 0.6534351145038167
+    startRateX: 0.0764872521246459,
+    startRateY: 0.5053435114503817,
+    endRateX: 0.9206798866855525,
+    endRateY: 0.6534351145038167,
 };
 
 const coordinate6 = {
-    startRateX: 0.11331444759206799, startRateY: 0.250381679389313, endRateX: 0.8725212464589235, endRateY: 0.650381679389313
+    startRateX: 0.11331444759206799,
+    startRateY: 0.250381679389313,
+    endRateX: 0.8725212464589235,
+    endRateY: 0.650381679389313,
 };
 
 const coordinate7 = {
-    startRateX: 0.06515580736543909, startRateY: 0.6473282442748092, endRateX: 0.28611898016997167, endRateY: 0.6977099236641221
+    startRateX: 0.06515580736543909,
+    startRateY: 0.6473282442748092,
+    endRateX: 0.28611898016997167,
+    endRateY: 0.6977099236641221,
 };
 
 const coordinate8 = {
-    startRateX: 0.059490084985835696, startRateY: 0.7923664122137405, endRateX: 0.9405099150141643, endRateY: 0.8641221374045801
+    startRateX: 0.059490084985835696,
+    startRateY: 0.7923664122137405,
+    endRateX: 0.9405099150141643,
+    endRateY: 0.8641221374045801,
 };
 
 const coordinate9 = {
-    startRateX: 0.9235127478753541, startRateY: 0.2580152671755725, endRateX: 0.0708215297450425, endRateY: 0.20916030534351146
+    startRateX: 0.9235127478753541,
+    startRateY: 0.2580152671755725,
+    endRateX: 0.0708215297450425,
+    endRateY: 0.20916030534351146,
 };
 
 const coordinate10 = {
-    startRateX: 0.6288951841359773, startRateY: 0.36946564885496186, endRateX: 0.5325779036827195, endRateY: 0.31603053435114503
+    startRateX: 0.6288951841359773,
+    startRateY: 0.36946564885496186,
+    endRateX: 0.5325779036827195,
+    endRateY: 0.31603053435114503,
 };
 
 const coordinate11 = {
-    startRateX: 0.6288951841359773, startRateY: 0.36946564885496186, endRateX: 0.5325779036827195, endRateY: 0.31603053435114503
+    startRateX: 0.6288951841359773,
+    startRateY: 0.36946564885496186,
+    endRateX: 0.5325779036827195,
+    endRateY: 0.31603053435114503,
 };
 
 const coordinate12 = {
-    startRateX: 0.9291784702549575, startRateY: 0.4534351145038168, endRateX: 0.0708215297450425, endRateY: 0.40458015267175573
+    startRateX: 0.9291784702549575,
+    startRateY: 0.4534351145038168,
+    endRateX: 0.0708215297450425,
+    endRateY: 0.40458015267175573,
 };
 
 const coordinate13 = {
-    startRateX: 0.9376770538243626, startRateY: 0.5954198473282443, endRateX: 0.4985835694050991, endRateY: 0.5374045801526718
+    startRateX: 0.9376770538243626,
+    startRateY: 0.5954198473282443,
+    endRateX: 0.4985835694050991,
+    endRateY: 0.5374045801526718,
 };
 
 const coordinate14 = {
-    startRateX: 0.9376770538243626, startRateY: 0.5954198473282443, endRateX: 0.4985835694050991, endRateY: 0.5374045801526718
+    startRateX: 0.9376770538243626,
+    startRateY: 0.5954198473282443,
+    endRateX: 0.4985835694050991,
+    endRateY: 0.5374045801526718,
 };
 
 const coordinate15 = {
-    startRateX: 0.5014164305949008, startRateY: 0.33435114503816793, endRateX: 0.9376770538243626, endRateY: 0.3969465648854962
+    startRateX: 0.5014164305949008,
+    startRateY: 0.33435114503816793,
+    endRateX: 0.9376770538243626,
+    endRateY: 0.3969465648854962,
 };
 
 const coordinate16 = {
     startRateX: 0,
     startRateY: 0,
     endRateX: 1,
-    endRateY: 1
+    endRateY: 1,
 };
 
 coordinateArray.push(
