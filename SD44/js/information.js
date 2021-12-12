2000aa609d6070fc4bcdd0148f90ed04a84dbd73
BIBLIC <92579317+BIBLIC@users.noreply.github.com>
2021-12-13 05:43:19 +0900

    SD44 초안 추가

--- SD44/js/information.js
+++ SD44/js/information.js
@@ -6,21 +6,21 @@ for (j = 1; j <= pageNum; j++) {
     audioArray.push(audio);
 }
 
-const page1 = "";
-const page2 = "";
-const page3 = "";
-const page4 = "";
-const page5 = "";
-const page6 = "";
-const page7 = "";
-const page8 = "";
-const page9 = "";
-const page10 = "";
-const page11 = "";
-const page12 = "";
-const page13 = "";
-const page14 = "";
-const page15 = "";
+const page1 = "디지털 범죄 - 보이스피싱 O.X. 퀴즈";
+const page2 = "통화를 듣고 보이스피싱을 찾아주세요. 화면을 눌러서 다음으로 진행해 주세요.";
+const page3 = "1번 통화 듣기 버튼을 누르면 통화 재생이 시작됩니다.";
+const page4 = "디지털 범죄 보이스피싱 1번 통화가 재생 중입니다.";
+const page5 = "1번 통화는 보이스 피싱입니다.";
+const page6 = "2번 통화 듣기 버튼을 누르면 통화 재생이 시작됩니다.";
+const page7 = "디지털 범죄 보이스피싱 2번 통화가 재생 중입니다.";
+const page8 = "2번 통화는 보이스 피싱입니다.";
+const page9 = "3번 통화 듣기 버튼을 누르면 통화 재생이 시작됩니다.";
+const page10 = "디지털 범죄 보이스피싱 3번 통화가 재생 중입니다.";
+const page11 = "3번 통화는 보이스 피싱입니다.";
+const page12 = "4번 통화 듣기 버튼을 누르면 통화 재생이 시작됩니다.";
+const page13 = "디지털 범죄 보이스피싱 4번 통화가 재생 중입니다.";
+const page14 = "4번 통화는 보이스 피싱입니다.";
+const page15 = "준비한 퀴즈를 모두 완료하셨습니다. 감사합니다.";
 
 pageDescArray.push(
     page1,
@@ -39,6 +39,28 @@ pageDescArray.push(
     page14,
     page15
 );
+
+
+const inputPage = [{ page: null, correctAnswer: null }];
+
+function handleInput(pageNumber) {
+    for (i = 0; i < inputPage.length; i++) {
+        if (pageNumber === inputPage[i].page) {
+            DrawInputBox(coordinateArray[pageNumber]);
+            let answer = inputPage[i].correctAnswer;
+            document.querySelector(".input-answer").addEventListener("keydown", function (event) {
+                if (event.keyCode === 15) {
+                    if (this.value === answer) {
+                        handleClickBox();
+                    } else if (this.value !== answer) {
+                        alert(RETRY_MESSAGE);
+                    }
+                }
+            });
+        }
+    }
+}
+
 
 const coordinate1 = {
     startRateX: 0,
