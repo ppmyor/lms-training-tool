3edcaee4480b417ec63b280d8a6e8f48ee54038a
BIBLIC <92579317+BIBLIC@users.noreply.github.com>
2021-12-21 10:16:46 +0900

    포팅테스트

--- common/js/app-potting.js
+++ common/js/app-potting.js
@@ -2,7 +2,7 @@
 var JS = document.createElement("script");
 JS.type = "text/javascript";
 JS.charset = "UTF-8";
-JS.src = "/common/js/content_tracking.js";
+JS.src = "https://ssc.seoul.kr/common/js/content_tracking.js";
 document.getElementsByTagName("head")[0].appendChild(JS);
 
 // Audio Section
